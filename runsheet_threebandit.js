e9c8946dd6e017bb10aa3f3de6c6c9d01d09ed5a
Henrike Flimm <Henrike.Flimm@psy.lmu.de>
2025-02-14 14:28:14 +0100

    Delete runsheet_threebandit.js

--- runsheet_threebandit.js
+++
@@ -1,8 +0,0 @@
-// specify stimuli
-var task_stimuli = [
-
-    {left_stim_number: 0, middle_stim_number: 1, right_stim_number: 2}
-    //{left_stim_number: 1, middle_stim_number: 2, right_stim_number: 0},
-    //{left_stim_number: 2, middle_stim_number: 0, right_stim_number: 1}
-];
-
